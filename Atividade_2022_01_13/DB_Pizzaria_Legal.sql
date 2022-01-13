71d828df9e1f6ff039c6f0a857474c785b5a26e7
Gabriel Luis Nolasco <94998727+BielIl@users.noreply.github.com>
2022-01-13 20:18:56 -0300

    Update DB_Pizzaria_Legal.sql

--- Atividade_2022_01_13/DB_Pizzaria_Legal.sql
+++ Atividade_2022_01_13/DB_Pizzaria_Legal.sql
@@ -4,8 +4,8 @@ USE db_pizzaria_legal;
 
 CREATE TABLE tb_categorias(
 	id_categoria_pizza BIGINT(5) AUTO_INCREMENT,
-    tipo_pizza VARCHAR(20),
-    PRIMARY KEY (id_categoria_pizza)
+    	tipo_pizza VARCHAR(20),
+    	PRIMARY KEY (id_categoria_pizza)
 );
 
 CREATE TABLE tb_pizzas(
@@ -23,20 +23,20 @@ CREATE TABLE tb_pizzas(
 
 INSERT INTO tb_categorias(tipo_pizza)
 	VALUES 	("Inteira"),
-			("Meia-Meia"),
-			("Broto"),
-			("Familia"),
-			("Pedaco");
+		("Meia-Meia"),
+		("Broto"),
+		("Familia"),
+		("Pedaco");
 
 INSERT INTO tb_pizzas(massa_pizza, sabor1_pizza, sabor2_pizza, sabor3_pizza, sabor4_pizza, valor_pizza, tipo_pizza)
 	VALUES  ("Nomral", "Calabresa", "Cheddar", null, null, 23.99, 2),
-			("Recheada", "Cheddar", null, null, null, 19.99, 1),
-            ("Recheada", "Frango", "Cheddar", "calabresa", null, 26.99, 4),
-            ("Nomral", "Frango", null, null, null, 19.99, 3),
-            ("Recheada", "Mussarela", "Margherita", null, null, 25.99, 2),
-            ("Nomral", "Mussarela", null, null, null, 9.99, 5),
-            ("Recheada", "Portuguesa", null, null, null, 14.99, 3),
-            ("Recheada", "Portuguesa", "Calabresa", "Margherita", "Moda da Casa", 41.99, 4);
+		("Recheada", "Cheddar", null, null, null, 19.99, 1),
+            	("Recheada", "Frango", "Cheddar", "calabresa", null, 26.99, 4),
+            	("Nomral", "Frango", null, null, null, 19.99, 3),
+            	("Recheada", "Mussarela", "Margherita", null, null, 25.99, 2),
+            	("Nomral", "Mussarela", null, null, null, 9.99, 5),
+            	("Recheada", "Portuguesa", null, null, null, 14.99, 3),
+            	("Recheada", "Portuguesa", "Calabresa", "Margherita", "Moda da Casa", 41.99, 4);
             
 SELECT * FROM tb_pizzas
 WHERE valor_pizza > 30;
