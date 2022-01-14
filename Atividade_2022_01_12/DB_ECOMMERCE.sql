020ad3a7e4b487f2efb53905026bc2f2d934139f
Gabriel Luis Nolasco <94998727+BielIl@users.noreply.github.com>
2022-01-14 15:09:21 -0300

    Update DB_ECOMMERCE.sql

--- Atividade_2022_01_12/DB_ECOMMERCE.sql
+++ Atividade_2022_01_12/DB_ECOMMERCE.sql
@@ -4,22 +4,22 @@ USE db_ecommerce;
 
 CREATE TABLE tb_produtos(
 	id_produto BIGINT(5) AUTO_INCREMENT,
-    nome_produto VARCHAR(20),
-    tipo_produto VARCHAR(20),
-    valor_produto DECIMAL(10,2),
-    validade_produto VARCHAR(10) DEFAULT NULL,
-    PRIMARY KEY (id_produto)
+   	nome_produto VARCHAR(20),
+    	tipo_produto VARCHAR(20),
+    	valor_produto DECIMAL(10,2),
+    	validade_produto VARCHAR(10) DEFAULT NULL,
+    	PRIMARY KEY (id_produto)
 );
 
 INSERT INTO tb_produtos(nome_produto, tipo_produto, valor_produto)
-VALUES 	("Revista", "Diversos", 16.99),
+VALUES 		("Revista", "Diversos", 16.99),
 		("Cigarro", "Diversos", 17.99),
-        ("Jogos Eletronicos", "E-Gamer", 199.99);
+        	("Jogos Eletronicos", "E-Gamer", 199.99);
         
 INSERT INTO tb_produtos(nome_produto, tipo_produto, valor_produto, validade_produto)
-VALUES	("Hot-Dog", "Alimentos", 14.99, "2022/01/14"),
+VALUES		("Hot-Dog", "Alimentos", 14.99, "2022/01/14"),
 		("Milho", "Alimentos", 6.99, "2022/04/11"),
-        ("Ervilha em Lata", "Enlatados", 4.99, "2022/09/11");
+        	("Ervilha em Lata", "Enlatados", 4.99, "2022/09/11");
         
 SELECT * FROM tb_produtos
 WHERE valor_produtos > 11.00;
