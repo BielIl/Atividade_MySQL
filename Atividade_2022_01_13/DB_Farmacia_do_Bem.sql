aaff41aeb54b07464f776c0a625476abb14d5ffa
Gabriel Luis Nolasco <94998727+BielIl@users.noreply.github.com>
2022-01-15 17:42:40 -0300

    Update DB_Farmacia_do_Bem.sql

--- Atividade_2022_01_13/DB_Farmacia_do_Bem.sql
+++ Atividade_2022_01_13/DB_Farmacia_do_Bem.sql
@@ -5,8 +5,8 @@ USE db_farmacia_do_bem;
 CREATE TABLE tb_categorias(
 
 	id_categoria BIGINT(5) AUTO_INCREMENT,
-    tipo_categoria VARCHAR(20),
-    setor_produto VARCHAR(4),
+    	tipo_categoria VARCHAR(20),
+    	setor_produto VARCHAR(4),
     
     PRIMARY KEY (id_categoria)
 );
@@ -14,32 +14,32 @@ CREATE TABLE tb_categorias(
 CREATE TABLE tb_produtos(
 
 	id_produto BIGINT(5) AUTO_INCREMENT,
-    nome_produto VARCHAR(64),
-    valor_produto DECIMAL(10,2),
+    	nome_produto VARCHAR(64),
+    	valor_produto DECIMAL(10,2),
     
-    PRIMARY KEY(id_produto),
+    	PRIMARY KEY(id_produto),
     
-    categoria_produto BIGINT,
-    FOREIGN KEY (categoria_produto) REFERENCES tb_categorias(id_categoria)
+    	categoria_produto BIGINT,
+    	FOREIGN KEY (categoria_produto) REFERENCES tb_categorias(id_categoria)
     
 );
 
 INSERT INTO tb_categorias(tipo_categoria, setor_produto)
-VALUES 	("Medicamento", "Se01"),
+VALUES 		("Medicamento", "Se01"),
 		("Medicamento", "Se02"),
-        ("Diverso", "Se06"),
-        ("Medicamento", "Se03"),
-        ("Alimento", "Se05");
+        	("Diverso", "Se06"),
+        	("Medicamento", "Se03"),
+        	("Alimento", "Se05");
         
 INSERT INTO tb_produtos(nome_produto, valor_produto, categoria_produto)
-VALUES 	("Clonazepan", 69.99, 1),
+VALUES 		("Clonazepan", 69.99, 1),
 		("Diazepam", 79.99, 1),
-        ("Cloridrato de Metformina", 39.99, 2),
-        ("Diclofenaco Sodico", 19.99, 4),
-        ("Camisinha", 0.99, 3),
-        ("Revista", 16.72, 3),
-        ("Barra Nutritiva", 3.99, 5),
-        ("Barra Sabor Banana", 2.99, 5);
+        	("Cloridrato de Metformina", 39.99, 2),
+        	("Diclofenaco Sodico", 19.99, 4),
+        	("Camisinha", 0.99, 3),
+        	("Revista", 16.72, 3),
+        	("Barra Nutritiva", 3.99, 5),
+        	("Barra Sabor Banana", 2.99, 5);
         
 SELECT * FROM tb_produtos
 WHERE valor_produto > 50;
